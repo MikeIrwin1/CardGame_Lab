db82433e67d2bafbbe5c954821b28a0a78847281
Michael Irwin <mike.irwin1@gmail.com>
2019-02-14 15:12:15 +0000

    populated the deck with 52 cards and shuffled them

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 
@@ -8,9 +9,33 @@ public class Deck {
         this.cards = new ArrayList<>();
     }
 
-
-
     public int numberOfCards() {
         return this.cards.size();
+    }
+
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public Card makeCard() {
+        Card card = new Card(SuitType.SPADES, RankType.ACE);
+        return card;
+    }
+
+    public void makeCardset(){
+        SuitType suits[] = SuitType.values();
+        RankType ranks[] = RankType.values();
+
+        for (SuitType suit : suits){
+            for (RankType rank : ranks){
+                Card card = new Card(suit, rank);
+                addCard(card);
+            }
+        }
+    }
+
+    public void shuffle(){
+        Collections.shuffle(this.cards);
     }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,19 +1,43 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class DeckTest {
 
     private Deck deck;
+    private Card card;
 
     @Before
     public void before(){
         deck = new Deck();
+        card = new Card(SuitType.SPADES, RankType.ACE);
     }
 
     @Test
     public void deckShouldStartEmpty(){
         assertEquals(0, deck.numberOfCards());
+    }
+
+    @Test
+    public void deckCanAddCardToList(){
+        deck.addCard(card);
+        assertEquals(1, deck.numberOfCards());
+    }
+
+    @Test
+    public void deckCanMakeCard(){
+        Card card = deck.makeCard();
+        deck.addCard(card);
+        assertEquals(1, deck.numberOfCards());
+    }
+
+    @Test
+    public void deckHas52Cards(){
+        deck.makeCardset();
+        deck.shuffle();
+        assertEquals(52, deck.numberOfCards());
     }
 }
